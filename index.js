2b4f608f64b081948cebca25b7c536f1d3164e0f
Utkarsh Paliwal <105985373+upaliwal1234@users.noreply.github.com>
2024-02-28 10:15:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const Product = require('./models/product');
 const Review = require('./models/review');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const dotenv = require('dotenv')
+
+dotenv.config();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -12,13 +15,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-// mongoose.connect('mongodb://127.0.0.1:27017/E-Commerce')
-//     .then(() => {
-//         console.log('Connected to MongoDB');
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     })
+app.get('/', (req, res) =>{
+    res.redirect('/products');
+})
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({});
@@ -74,12 +73,9 @@ app.post('/products/:id', async (req, res) => {
     res.redirect(`/products/${productId}`);
 })
 
-const PORT = 5500;
-// app.listen(PORT, () => {
-//     console.log('server is up at port', PORT);
-// })
+const PORT = process.env.PORT || 5500;
 
-mongoose.connect('mongodb+srv://admin:admin1234@practiceapi.us7xhdx.mongodb.net/Node-API?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(PORT, () => {
             console.log('server is up at port', PORT);
